52929e5ddb16f78c46bb142925605be4aaafc3c7
vibesx <lucian.paslariu@gmail.com>
2022-11-30 15:04:37 +0200

    added lots of stuff

--- animations--01-start/src/App.js
+++ animations--01-start/src/App.js
@@ -1,23 +1,127 @@
 import React, { Component } from "react";
+import { useState } from "react";
+import Transition from "react-transition-group/Transition";
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>React Animations</h1>
-        <Modal />
-        <Backdrop />
-        <button className="Button">Open Modal</button>
-        <h3>Animating Lists</h3>
-        <List />
-      </div>
-    );
-  }
+// class App extends Component {
+// 	state = { modalIsOpen: false };
+
+// 	showModal = () => {
+// 		console.log("show");
+// 		this.setState({ modalIsOpen: true });
+// 	};
+
+// 	closeModal = () => {
+// 		console.log("close");
+// 		this.setState({ modalIsOpen: false });
+// 	};
+
+// 	render() {
+// 		return (
+// 			<div className="App">
+// 				<h1>React Animations</h1>
+// 				<Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+// 				<Backdrop show={this.state.modalIsOpen} />
+// 				<button className="Button" onClick={this.showModal}>
+// 					Open Modal
+// 				</button>
+// 				<h3>Animating Lists</h3>
+// 				<List />
+// 			</div>
+// 		);
+// 	}
+// }
+
+// in order to be able to pass this object to the timeout property of Transition, it needs to have these 2 fields: enter and exit,
+// both being the time in miliseconds
+const animationTiming = {
+	enter: 400,
+	exit: 1000,
+};
+
+function App(props) {
+	const [modalIsOpen, setModalIsOpen] = useState(false);
+	const [showBlock, setShowBlock] = useState(false);
+
+	const showModalHandler = () => {
+		setModalIsOpen(true);
+	};
+
+	const closeModalHandler = () => {
+		setModalIsOpen(false);
+	};
+
+	return (
+		<div className="App">
+			<h1>React Animations</h1>
+			<button
+				className="Button"
+				onClick={() => {
+					setShowBlock((prevState) => {
+						return !prevState;
+					});
+				}}
+			>
+				Toggle
+			</button>
+			<br />
+			{/* mountOnEnter mounts this component in the DOM when it is rendered by the transition; unmountOnExit does the opposite, it removes it from the DOM on exit */}
+			{/* timeout represents how long the transition will last, but it overrides the timeout of our css animation,
+      so if we define an animation of 1000ms and set timeout to 300ms, it will cut the last 700ms of the animation */}
+			{/* we can use onEvent properties to define certain behaviours we want on certain events;
+      the order of events is: enter -> entering -> entered (after the timeout period); same applies to exit */}
+			<Transition
+				in={showBlock}
+				timeout={300}
+				mountOnEnter
+				unmountOnExit
+				onEnter={() => console.log("Enter")}
+				onEntering={() => console.log("Entering")}
+				onEntered={() => console.log("Entered")}
+				onExit={() => console.log("Exit")}
+				onExiting={() => console.log("Exiting")}
+				onExited={() => console.log("Exited")}
+			>
+				{/* state is a property returned by Transition, which can be Entering, Entered, Exiting, Exited */}
+				{(state) => {
+					return (
+						<div
+							style={{
+								backgroundColor: "red",
+								width: 100,
+								height: 100,
+								margin: "auto",
+								transition: "opacity 1s ease-out",
+								opacity: state === "exiting" ? 0 : 1,
+							}}
+						></div>
+					);
+				}}
+			</Transition>
+			{/* we can move the transition up a level and wrap the JSX code returned by Modal in Transition and refactor */}
+			{/* an alternative to Transition is CSSTransition, which is a bit more elegant; check docs online */}
+			<Transition
+				in={modalIsOpen}
+				timeout={animationTiming}
+				mountOnEnter
+				unmountOnExit
+			>
+				{(state) => {
+					return <Modal show={state} closed={closeModalHandler} />;
+				}}
+			</Transition>
+			{modalIsOpen && <Backdrop show />}
+			<button className="Button" onClick={showModalHandler}>
+				Open Modal
+			</button>
+			<h3>Animating Lists</h3>
+			<List />
+		</div>
+	);
 }
 
 export default App;
--- animations--01-start/src/components/Modal/Modal.js
+++ animations--01-start/src/components/Modal/Modal.js
@@ -1,12 +1,54 @@
-import React from 'react';
+import React from "react";
 
-import './Modal.css';
+import "./Modal.css";
 
-const modal = (props) => (
-    <div className="Modal">
-        <h1>A Modal</h1>
-        <button className="Button" onClick={props.closed}>Dismiss</button>
-    </div>
-);
+const modal = (props) => {
+	const cssClasses = [
+		"Modal",
+		props.show === "entering"
+			? "ModalOpen"
+			: props.show === "exiting"
+			? "ModalClose"
+			: null,
+	];
+	const onClickHandler = () => {
+		props.closed();
+	};
+	return (
+		<div className={cssClasses.join(" ")}>
+			<h1>A Modal</h1>
+			<button className="Button" onClick={onClickHandler}>
+				Dismiss
+			</button>
+		</div>
+	);
+};
 
 export default modal;
+
+// CSSTransition wrapping method:
+// import CSSTransition from "react-transition-group/CSSTransition";
+
+// <CSSTransition
+//         mountOnEnter
+//         unmountOnExit
+//         in={props.show}
+//         timeout={animationTiming}
+//         classNames={{
+//             enter: '',
+//             enterActive: 'ModalOpen',
+//             exit: '',
+//             exitActive: 'ModalClosed'
+//         }}>
+//           <div className="Modal">
+//             <h1>A Modal</h1>
+//             <button className="Button" onClick={props.closed}>
+//               Dismiss
+//             </button>
+//           </div>
+//     </CSSTransition>
+
+// classNames can also take a string as a value and require us to define 4 classes in our css files: <our_value>-enter, -enter-active, -exit, -exit-active
+// so in our css we should have, for a value of our-modal:
+// .our-modal-enter, .our-modal-enter-active, .our-modal-exit, .our-modal-exit-active
+// these will then be automatically recognized and applied at the right time of transition
